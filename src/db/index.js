2b049d6d9559501298806f63e59a80e02432bc32
Oscar Ruiz <oscaralexruiz@gmail.com>
2021-06-20 20:49:29 -0500

    auto schema

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,16 @@ const sqlite3 = require('sqlite3').verbose()
 class Database{
 	connection = null
 
+	#getAllTableNamesQuery = /*sql*/`
+	SELECT 
+		name
+	FROM 
+		sqlite_master 
+	WHERE 
+		type ='table' AND 
+		name NOT LIKE 'sqlite_%';
+	`
+
 	#pathDB = './src/db/store.db'
 	#InitialSchema = [
 		{
@@ -20,10 +30,38 @@ class Database{
 
 	constructor(){
 		this.connection = new sqlite3.Database(this.#pathDB)
+
+		this.#checkSchema()
+		
 	}
 
-	checkSchema(){
-		
+	#checkSchema(){
+		new Promise((resolve, reject)=>{
+			this.#getCurrentTables().then(tables=>{
+				// TODO generar schema automatico
+				// this.connection.run('CREATE TABLE commits (id INTEGER)')
+				if(tables.length > 0){
+					this.#InitialSchema.forEach(table=>{
+						
+					})
+				}else{
+
+				}
+			}).catch(err=>{
+				reject(err);
+			})
+		})
+	}
+
+	#getCurrentTables(){
+		return new Promise((resolve, reject)=>{
+			this.connection.all(this.#getAllTableNamesQuery,[],(err,results)=>{
+				if(err) reject(err);
+				else{
+					resolve(results.map(table=>table.name))
+				}
+			})
+		})
 	}
 }
 
